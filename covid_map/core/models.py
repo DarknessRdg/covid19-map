7af81dc9eaa4fb578997a425c589b5da7dd5f9ea
Luan Rodrigues <luanrodrigues007@hotmail.com>
2020-05-16 22:29:33 -0300

    add update model

--- covid_map/core/models.py
+++ covid_map/core/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
 class CasosPorCidadePiaui(models.Model):
     """Model definition for CasosPorCidadePiaui."""
 
@@ -18,3 +18,29 @@ class CasosPorCidadePiaui(models.Model):
     def __str__(self):
         """Unicode representation of CasosPorCidadePiaui."""
         return '{}'.format(self.name)
+
+
+class UpdateData(models.Model):
+    """
+    Model to store file name present on folder `/scraps/{name}.py`. Attribute name should be a human readable
+    name. The spaces will be replaced for an underscore when loading file.
+
+    An example of implementation:
+    # script to update
+    - core/
+        - scraps/
+            - sao_paulo.py  # file names is created lower case and snake case.
+                - main()
+
+    # model created
+    UpdateData(name='Sao Paulo').save()
+    """
+    name = models.CharField(help_text='File name to be ran function main()', unique=True, max_length=255)
+    active = models.BooleanField(default=True)
+
+    @property
+    def file_name(self):
+        return self.name.lower().replace(' ', '_')
+
+    def __str__(self):
+        return self.name.capitalize()
